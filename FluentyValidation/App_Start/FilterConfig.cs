0c4f308f5fec187ecdc46818dfec0b7640564f37
Chuck Bryan <cbryan@marathonus.com>
2016-05-09 13:42:57 -0400

    Add project files.

---
+++ FluentyValidation/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace FluentyValidation
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ FluentyValidation/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FluentyValidation.MvcApplication" Language="C#" %>
